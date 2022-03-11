8f1f8b225441cff477ce402983db09bdae9cf02b
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-12 01:26:56 +0900

    update

--- scripts/generate-env.sh
+++ scripts/generate-env.sh
@@ -4,8 +4,6 @@ set -eux
 
 cd $(dirname $0)/../
 
-mkdir -p config
-
 export BUILD_DATE=$(date "+%Y-%m-%d-%H-%M")
 
-envsubst < ./scripts/Env.xcconfig.template > ./config/Env.xcconfig
+envsubst < ./Env.xcconfig.template > ./Env.xcconfig
