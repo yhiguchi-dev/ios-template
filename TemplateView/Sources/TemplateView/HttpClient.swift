68bc73332c6c0e1c9f7ba6fc900da8c9379e731e
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-18 01:29:58 +0900

    update

--- TemplateView/Sources/TemplateView/HttpClient.swift
+++ TemplateView/Sources/TemplateView/HttpClient.swift
@@ -5,20 +5,10 @@
 import Foundation
 
 struct HttpClient {
-
-  func send<R: Decodable>(request: URLRequest) async throws -> R? {
-    let (data, response) = try await URLSession.shared.data(for: request)
-    switch try HttpStatus.of(code: response.statusCode) {
-    case .Successful:
-      if let data = data {
-        let decoder = JSONDecoder()
-        let responseBody = try decoder.decode(R.self, from: data)
-        return responseBody
-      }
-      return nil
-    case .ClientError, .ServerError:
-      return nil
-    }
+  static func send<T: Encodable, R: Decodable>(url: String, requestBody: T?) async throws -> R? {
+    let urlRequest = try HttpRequestBuilder.build(url: url, requestBody: requestBody)
+    let (data, response) = try await URLSession.shared.data(for: urlRequest)
+    return try HttpResponseResolver<R>.resolve(code: response.statusCode, data: data)
   }
 }
 
@@ -38,4 +28,52 @@ extension URLSession {
       dataTask.resume()
     }
   }
+}
+
+struct HttpRequestBuilder<T: Encodable> {
+  static func build(url: String, requestBody: T?) throws -> URLRequest {
+    guard let url = URL(string: url) else {
+      throw HttpClientError.invalidUrl
+    }
+    if let requestBody = requestBody {
+      var urlRequest = URLRequest(url: url)
+      let encoder = JSONEncoder()
+      urlRequest.httpBody = try encoder.encode(requestBody)
+      return urlRequest
+    }
+    return URLRequest(url: url)
+  }
+}
+
+struct HttpResponseResolver<R: Decodable> {
+  static func resolve(code: Int, data: Data?) throws -> R? {
+    switch code {
+    case (200..<299):
+      if let data = data {
+        let decoder = JSONDecoder()
+        let responseBody = try decoder.decode(R.self, from: data)
+        return responseBody
+      }
+      return nil
+    case (400..<499):
+      if let data = data, let message = String(data: data, encoding: .utf8) {
+        throw HttpClientError.clientError(code: code, message: message)
+      }
+      throw HttpClientError.clientError(code: code)
+    case (500..<599):
+      if let data = data, let message = String(data: data, encoding: .utf8) {
+        throw HttpClientError.serverError(code: code, message: message)
+      }
+      throw HttpClientError.serverError(code: code)
+    default:
+      throw HttpClientError.unknownStatusCodeError(code: code)
+    }
+  }
+}
+
+enum HttpClientError: Error {
+  case invalidUrl
+  case clientError(code: Int, message: String? = nil)
+  case serverError(code: Int, message: String? = nil)
+  case unknownStatusCodeError(code: Int)
 }
