34e828dc780c4f2cd3f79db7931caddb9591a094
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-26 17:13:18 +0900

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,13 +2,19 @@
 
 set -eux
 
-cd $(dirname $0)/../
+cd "$(dirname "$0")"/../
 
 scheme=TemplateView
 destination='platform=iOS Simulator,name=iPhone 8,OS=15.2'
 
+rm -rf TestReports
+
+mkdir -p TestReports TestReports/${scheme}
+
 cd ${scheme}
 
-rm -rf TestResults TestResults.xcresult
+xcodebuild test -scheme ${scheme} -destination "${destination}" -derivedDataPath "${DERIVED_DATA_PATH}" -resultBundlePath ../TestReports/${scheme}/TestResults
 
-xcodebuild test -scheme ${scheme} -destination "${destination}" -derivedDataPath ${DERIVED_DATA_PATH} -resultBundlePath TestResults
+cd ../
+
+swift run xchtmlreport -r TestReports/${scheme}/TestResults
